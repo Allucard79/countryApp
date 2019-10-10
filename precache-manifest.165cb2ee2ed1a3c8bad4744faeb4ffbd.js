4e749a3d9e60ab3d79a40efdaef60bbd8fd4a9b2
Marcin Ratajczak <maratajczak@gmail.com>
2019-10-10 21:07:04 +0200

    Updates

---
+++ precache-manifest.165cb2ee2ed1a3c8bad4744faeb4ffbd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/countryApp/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "9a975b84ca089417f851",
+    "url": "/countryApp/static/css/main.a5f138fd.chunk.css"
+  },
+  {
+    "revision": "1c6e0d88b6c93a8ff901",
+    "url": "/countryApp/static/js/runtime~main.1c6e0d88.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/countryApp/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "5b5471caf1e9a331d1f3",
+    "url": "/countryApp/static/js/2.5b5471ca.chunk.js"
+  },
+  {
+    "revision": "9a975b84ca089417f851",
+    "url": "/countryApp/static/js/main.9a975b84.chunk.js"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/countryApp/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/countryApp/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/countryApp/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "5b5471caf1e9a331d1f3",
+    "url": "/countryApp/static/css/2.24656401.chunk.css"
+  },
+  {
+    "revision": "91bbd05f75c9652d9c8ef527025a4fd2",
+    "url": "/countryApp/index.html"
+  }
+];
